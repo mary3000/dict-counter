10cf899a6be249556bea4ac9e2636d7e9aeb50e6
Mary Feofanova <mary.fifa3000@gmail.com>
2022-08-18 20:37:10 +0300

    add solution

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # dict-counter
+
+---
+## todo
+
+- Dockerize
+- Tests
+- Common function with std::streams instead of files (needed for tests)
+- Cmd line args
+- 
---
+++ main.cpp
@@ -0,0 +1,75 @@
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <string>
+#include <unordered_map>
+
+#include <fmt/core.h>
+
+void MakeDict(std::istream& is, std::ostream& os) {
+  std::string line;
+
+  std::unordered_map<std::string, uint64_t> dict;
+
+  if (!is.eof()) {
+    while (std::getline(is, line)) {
+      std::regex e("[^a-zA-Z]+");
+      std::regex_token_iterator<std::string::iterator> i(line.begin(),
+                                                         line.end(), e, -1);
+      std::regex_token_iterator<std::string::iterator> end;
+      while (i != end) {
+        std::string str = *i++;
+        std::transform(str.begin(), str.end(), str.begin(),
+                       [](unsigned char c) { return std::tolower(c); });
+        dict[str]++;
+      }
+    }
+
+    struct Token {
+      std::string word;
+      uint64_t count;
+
+      Token(std::string word_, uint64_t count_)
+          : word(std::move(word_)), count(count_) {}
+
+      bool operator<(const Token &other) const {
+        return count > other.count ||
+               (count == other.count && word < other.word);
+      }
+    };
+
+    std::vector<Token> tokens;
+    tokens.reserve(dict.size());
+    for (auto &[word, count] : dict) {
+      if (word.empty()) {
+        continue;
+      }
+      tokens.emplace_back(word, count);
+    }
+
+    std::sort(tokens.begin(), tokens.end());
+
+    for (auto& token : tokens) {
+      os << token.count << " " << token.word << std::endl;
+    }
+  } else {
+    throw std::runtime_error("Couldn't open the file");
+  }
+}
+
+void MakeDict(const std::string &in, const std::string& out) {
+  std::ifstream ifs(in, std::ifstream::in);
+  std::ofstream ofs(out, std::ifstream::out);
+  MakeDict(ifs, ofs);
+}
+
+int main(int argc, char *argv[]) {
+  const int arg_count = 2;
+  if (argc != arg_count + 1) {
+    throw std::runtime_error(fmt::format("Wrong number of arguments, expected {}, got {}", arg_count, argc - 1));
+  }
+
+  MakeDict(argv[1], argv[2]);
+
+  return 0;
+}
